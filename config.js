3cfb6c0e716b7a127a1e2e1dd30ad5dfd93a2761
Federico <violante.f@gmail.com>
2025-04-27 16:36:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://048b637a-1f03-4744-8df9-683da1a67780.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
+          pattern: "/provider/[^/]+",
+          metaDataEndpoint: "https://gist.githubusercontent.com/vfede/408bbc3b2ea4f3b5524e8f834b125d49/raw/d30292d6be8f045cd7db23ab39fd7ab79aa5780a/gistfile1.txt?{id}"
       },
       {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+          pattern: "/experience/[^/]+",
+          metaDataEndpoint: "https://gist.githubusercontent.com/vfede/408bbc3b2ea4f3b5524e8f834b125d49/raw/d30292d6be8f045cd7db23ab39fd7ab79aa5780a/gistfile1.txt?{id}"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
