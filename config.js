9f72d6bef99135feaf7fb5254d239d7981e9f446
vfede <violante.f@gmail.com>
2025-05-05 19:47:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 export const config = {
-  domainSource: "https://048b637a-1f03-4744-8df9-683da1a67780.weweb-preview.io", // Your WeWeb app preview link
-  patterns: [
-      {
-          pattern: "/provider/[^/]+",
-          metaDataEndpoint: "https://gist.githubusercontent.com/vfede/408bbc3b2ea4f3b5524e8f834b125d49/raw/d30292d6be8f045cd7db23ab39fd7ab79aa5780a/gistfile1.txt?{id}"
-      },
-      {
-          pattern: "/experience/[^/]+",
-          metaDataEndpoint: "https://gist.githubusercontent.com/vfede/408bbc3b2ea4f3b5524e8f834b125d49/raw/d30292d6be8f045cd7db23ab39fd7ab79aa5780a/gistfile1.txt?{id}"
-      }
-      // Add more patterns and their metadata endpoints as needed
-  ]
+    domainSource: "https://048b637a-1f03-4744-8df9-683da1a67780.weweb-preview.io", // Your WeWeb app preview link
+    patterns: [
+        {
+            pattern: "/provider/[^/]+",
+            metaDataEndpoint: "https://lccdbeogxyozaxntzhqe.supabase.co/functions/v1/getMetaExperience/{slug}"
+        },
+        {
+            pattern: "/experience/[^/]+",
+            metaDataEndpoint: "https://lccdbeogxyozaxntzhqe.supabase.co/functions/v1/getMetaExperience/{slug}"
+        }
+        // Add more patterns and their metadata endpoints as needed
+    ]
 };
