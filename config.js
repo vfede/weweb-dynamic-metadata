07443b5a1b269d72ee9a40b33cf3ca78c977016a
vfede <violante.f@gmail.com>
2025-05-05 22:35:40 +0200

    Match only the page url, not its styles and scripts

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ export const config = {
     domainSource: "https://048b637a-1f03-4744-8df9-683da1a67780.weweb-preview.io", // Your WeWeb app preview link
     patterns: [
         {
-            pattern: "/provider/[^/]+",
+            pattern: "^/provider/[^/]+",
             metaDataEndpoint: "https://lccdbeogxyozaxntzhqe.supabase.co/functions/v1/getMetaProvider/{slug}"
         },
         {
-            pattern: "/experience/[^/]+",
+            pattern: "^/experience/[^/]+",
             metaDataEndpoint: "https://lccdbeogxyozaxntzhqe.supabase.co/functions/v1/getMetaExperience/{slug}"
         }
         // Add more patterns and their metadata endpoints as needed
