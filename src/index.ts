bd8060b8bbf01a7a812d89eb702202afe0f69266
vfede <violante.f@gmail.com>
2025-05-05 19:42:19 +0200

    Add log

--- src/index.ts
+++ src/index.ts
@@ -63,6 +63,8 @@ export default {
       // Construct the final URL
       // const finalUrl = `https://lccdbeogxyozaxntzhqe.supabase.co/functions/v1/getMetaExperience/${id}`;
 
+      console.log("MetaData Endpoint:", metaDataEndpointWithId);
+
       // Fetch metadata using the updated URL
       try {
         const metaDataResponse = await fetch(metaDataEndpointWithId, {
